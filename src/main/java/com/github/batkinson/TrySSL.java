8a9c53d4cdbaaf898ad7bdd6c9009fb4195fc7db
Brent Atkinson <brent.atkinson@gmail.com>
2015-02-06 13:05:08 -0500

    Added ability to toggle cert/host validation via command line options.

--- src/main/java/com/github/batkinson/TrySSL.java
+++ src/main/java/com/github/batkinson/TrySSL.java
@@ -1,10 +1,10 @@
 package com.github.batkinson;
 
 
+import org.apache.commons.cli.*;
+
 import javax.net.ssl.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.security.KeyManagementException;
@@ -15,15 +15,37 @@ import java.util.Map;
 
 public class TrySSL {
 
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, KeyManagementException {
+    private static void printHelp(Options opts) {
+        HelpFormatter helpFormatter = new HelpFormatter();
+        String usage = String.format("java %s [-C] [-H] [-h] <https-url>", TrySSL.class.getName());
+        PrintWriter w = new PrintWriter(System.err);
+        helpFormatter.printHelp(usage, opts);
+        w.flush();
+    }
 
-        if (args.length != 1) {
-            System.err.printf("Usage: java %s <secure-url>", TrySSL.class.getName());
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, KeyManagementException, ParseException {
+
+        Options clOpts = new Options();
+        clOpts.addOption("C", "valid-certs", false, "check certificates (off by default)");
+        clOpts.addOption("H", "valid-hosts", false, "check hostnames (off by default)");
+        clOpts.addOption("h", "help", false, "prints this information");
+        CommandLineParser clParser = new GnuParser();
+
+        boolean trustAllCerts = false;
+        boolean trustAllHostnames = false;
+        try {
+            CommandLine cl = clParser.parse(clOpts, args);
+            trustAllCerts = !cl.hasOption("C");
+            trustAllHostnames = !cl.hasOption("H");
+            if (cl.hasOption("h")) {
+                printHelp(clOpts);
+                return;
+            }
+        } catch (ParseException pe) {
+            System.err.println(pe.getMessage());
+            printHelp(clOpts);
             System.exit(1);
         }
-
-        boolean trustAllCerts = true;
-        boolean trustAllHostnames = true;
 
         if (trustAllCerts) {
             // Install a trust manager that trusts all certificates
